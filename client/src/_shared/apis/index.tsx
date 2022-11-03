6db22321ef3bba5b75e44dd76be0bfdba4935b0d
youthfulhps <ybh942002@gmail.com>
2022-11-03 11:24:22 +0900

    Feature: Implement axios default instances

---
+++ client/src/_shared/apis/index.tsx
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import cookie from 'cookiejs';
+
+export const getAccessToken = () => {
+  return cookie.get('gitin:token');
+};
+
+export const axiosInstance = axios.create({
+  baseURL: process.env.API_BASE_PATH,
+  headers: {},
+});
+
+export const authAxiosInstance = axios.create({
+  baseURL: process.env.AUTH_BASE_URL,
+  headers: {},
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  if (!config || !config.headers) {
+    throw new Error(
+      "Expected 'config' and 'config.headers' not to be undefined"
+    );
+  }
+
+  const accessToken = getAccessToken();
+
+  if (!accessToken) {
+    const { CancelToken } = axios;
+    return {
+      ...config,
+      cancelToken: new CancelToken((cancel) =>
+        cancel('Access token is required!')
+      ),
+    };
+  }
+
+  config.headers.Authorization = `Token ${accessToken}`;
+
+  return config;
+});
