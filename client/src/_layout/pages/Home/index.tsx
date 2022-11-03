3074e0a97b03605c21856a6cc93f9404d2a2c752
youthfulhps <ybh942002@gmail.com>
2022-11-03 11:26:03 +0900

    Enhance: Migrate pages folder to shared layout

---
+++ client/src/_layout/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import GridMain from '@layout/components/GridMain';
+import Login from '../../../Login/components';
+
+function HomePage() {
+  return (
+    <GridMain>
+      <Login />
+    </GridMain>
+  );
+}
+
+export default HomePage;
