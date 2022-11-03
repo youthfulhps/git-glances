0d3215db624d9a29d88a08b6733132b7dd0e6346
youthfulhps <ybh942002@gmail.com>
2022-11-03 11:28:19 +0900

    Feature: Implement login component

--- client/src/Login/components/index.tsx
+++ client/src/Login/components/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { LogoGithub } from '@carbon/icons-react';
+import Section from '@layout/components/Section';
 
 function Login() {
-  const loginURL = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_OAUTH_CLIENT_ID}&redirect_uri=${process.env.GITHUB_OAUTH_REDIRECT_URL}`;
+  const loginURL =
+    `https://github.com/login/oauth/authorize` +
+    `?client_id=${process.env.GITHUB_OAUTH_CLIENT_ID}` +
+    `&redirect_uri=${process.env.GITHUB_OAUTH_REDIRECT_URL}`;
+
   return (
-    <a className="text-zinc-100" href={loginURL}>
-      로그인 합니다.
-    </a>
+    <Section>
+      <a className="text-zinc-100" href={loginURL}>
+        <LogoGithub />
+      </a>
+    </Section>
   );
 }
 
