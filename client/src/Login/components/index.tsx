9b558f4c8e0c76367394aef712143a634d89e1f8
youthfulhps <ybh942002@gmail.com>
2022-11-05 18:04:07 +0900

    Feature: Implement github login section components

--- client/src/Login/components/index.tsx
+++ client/src/Login/components/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import tw from 'tailwind-styled-components';
 import { LogoGithub } from '@carbon/icons-react';
 import Section from '@layout/components/Section';
+
+// TODO: any를 사용하지 않고 TS2589 이슈 해결
+const StyledLogoGithub = tw.a<any>`
+  h-full
+  w-full
+  flex
+  items-center
+  justify-center
+  rounded-2xl 
+  bg-zinc-400
+  hover:bg-zinc-100
+  transition-all
+`;
 
 function Login() {
   const loginURL =
@@ -9,10 +23,10 @@ function Login() {
     `&redirect_uri=${process.env.GITHUB_OAUTH_REDIRECT_URL}`;
 
   return (
-    <Section>
-      <a className="text-zinc-100" href={loginURL}>
-        <LogoGithub />
-      </a>
+    <Section className="grid-in-login">
+      <StyledLogoGithub href={loginURL}>
+        <LogoGithub size={32} />
+      </StyledLogoGithub>
     </Section>
   );
 }
