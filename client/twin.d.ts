8f091b95d60b08924e98744f714d9b0abdddf36f
youthfulhps <ybh942002@gmail.com>
2022-11-11 16:45:12 +0900

    Setting: Add twin declare types

---
+++ client/twin.d.ts
@@ -0,0 +1,24 @@
+import 'twin.macro';
+import styledImport, { CSSProp, css as cssImport } from 'styled-components';
+
+declare module 'twin.macro' {
+  const styled: typeof styledImport;
+  const css: typeof cssImport;
+}
+declare module 'react' {
+  interface HTMLAttributes<T> extends DOMAttributes<T> {
+    css?: CSSProp;
+  }
+
+  interface SVGProps<T> extends SVGProps<SVGSVGElement> {
+    css?: CSSProp;
+  }
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicAttributes<T> extends DOMAttributes<T> {
+      as?: string;
+    }
+  }
+}
