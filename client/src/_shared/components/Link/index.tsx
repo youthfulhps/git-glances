19e9b25c76a5db6a790eea4e761a89325b586074
youthfulhps <ybh942002@gmail.com>
2022-11-14 00:16:56 +0900

    Feature: Create link common component

---
+++ client/src/_shared/components/Link/index.tsx
@@ -0,0 +1,23 @@
+import { AnchorHTMLAttributes } from 'react';
+import tw from 'twin.macro';
+import styled from 'styled-components';
+
+type LinkProps = AnchorHTMLAttributes<HTMLAnchorElement>;
+
+const StyledLink = styled.a`
+  ${tw`rounded-2xl border border-solid border-zinc-300`};
+  ${tw`text-zinc-300`};
+  ${tw`p-2`};
+  ${tw`backdrop-blur`};
+  ${tw`hover:text-zinc-400`};
+`;
+
+function Link({ children, ...anchorProps }: LinkProps) {
+  return (
+    <StyledLink {...anchorProps} target="_blank">
+      {children}
+    </StyledLink>
+  );
+}
+
+export default Link;
