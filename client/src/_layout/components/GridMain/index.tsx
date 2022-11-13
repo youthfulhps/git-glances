1639016d4fbb42d0a1f2836fcb38232d615efd2f
youthfulhps <ybh942002@gmail.com>
2022-11-14 00:07:34 +0900

    Enhance: Replace style module of layout components

--- client/src/_layout/components/GridMain/index.tsx
+++ client/src/_layout/components/GridMain/index.tsx
@@ -1,23 +1,35 @@
 import { ReactNode } from 'react';
-import tw from 'tailwind-styled-components';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import useLogin from '@shared/hooks/useLogin';
 
 type GridMainProps = {
   children: ReactNode;
 };
 
-const StyledGridMain = tw.main`
-  grid 
-  h-screen 
-  w-screen 
-  grid-cols-layout 
-  grid-rows-layout 
-  gap-4 
-  p-32 
-  grid-areas-layout
+type StyledGridMainProps = {
+  isLoggedIn: boolean;
+};
+
+const StyledGridMain = styled.main<StyledGridMainProps>`
+  ${tw`h-screen w-screen gap-4`}
+  ${tw`p-32`};
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(6, 1fr);
+  grid-template-areas: ${({ isLoggedIn }) => `'${
+    isLoggedIn ? 'user-profile' : 'login'
+  } . . .'
+    'user-profile . . .'
+    'user-profile . . .'
+    '. . . .'
+    '. . . .'
+    '. . . .'`};
 `;
 
 function GridMain({ children }: GridMainProps) {
-  return <StyledGridMain>{children}</StyledGridMain>;
+  const { isLoggedIn } = useLogin();
+  return <StyledGridMain isLoggedIn={isLoggedIn}>{children}</StyledGridMain>;
 }
 
 export default GridMain;
--- client/src/_layout/components/Section/index.tsx
+++ client/src/_layout/components/Section/index.tsx
@@ -1,20 +1,28 @@
 import { ReactNode } from 'react';
-import tw from 'tailwind-styled-components';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+
+type StyledSectionProps = {
+  gridArea?: string;
+};
 
 type SectionProps = {
   children: ReactNode;
   className?: string;
-};
+} & StyledSectionProps;
 
-const StyledSection = tw.section`
-  h-full 
-  w-full 
-  rounded-2xl 
-  bg-zinc-800 
+const StyledSection = styled.section<StyledSectionProps>`
+  ${tw`h-full w-full rounded-2xl`}
+  ${tw`bg-zinc-800`}
+  grid-area: ${({ gridArea }) => `${gridArea}`};
 `;
 
-function Section({ children, className }: SectionProps) {
-  return <StyledSection className={className}>{children}</StyledSection>;
+function Section({ children, className, gridArea }: SectionProps) {
+  return (
+    <StyledSection className={className} gridArea={gridArea}>
+      {children}
+    </StyledSection>
+  );
 }
 
 export default Section;
