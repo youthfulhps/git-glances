7f9b50aa5d88c874b153e730b02997a1fd115e40
youthfulhps <ybh942002@gmail.com>
2022-11-14 00:18:57 +0900

    Feature: Apply suspense boundary at user profile section

--- client/src/_layout/pages/Home/index.tsx
+++ client/src/_layout/pages/Home/index.tsx
@@ -1,10 +1,15 @@
 import GridMain from '@layout/components/GridMain';
+import SuspenseBoundary from '@shared/boundaries/SuspenseBoundary';
 import Login from '../../../Login/components';
+import UserProfile from '../../../UserProfile/components';
 
 function HomePage() {
   return (
     <GridMain>
       <Login />
+      <SuspenseBoundary gridArea="user-profile">
+        <UserProfile />
+      </SuspenseBoundary>
     </GridMain>
   );
 }
