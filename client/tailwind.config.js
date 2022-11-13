ae1eaf1529dcc310624557820a57059193040233
youthfulhps <ybh942002@gmail.com>
2022-11-14 00:05:05 +0900

    Style: Add tailwind custom theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,16 +3,23 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      gridTemplateAreas: {
-        layout: ['login . .', '. . .', '. . .', '. . .', '. . .', '. . .'],
+      backdropGrayscale: {
+        40: '.4',
       },
-      gridTemplateColumns: {
-        layout: 'repeat(3, 1fr)',
+      colors: {
+        'black-rgba': 'rgba(0, 0, 0, 0.54)',
       },
-      gridTemplateRows: {
-        layout: 'repeat(6, 1fr)',
+      opacity: {
+        10: '.10',
+        20: '.20',
+        30: '.30',
+        40: '.40',
+        50: '.50',
+        60: '.60',
+        70: '.70',
+        80: '.80',
+        90: '.90',
       },
     },
   },
-  plugins: [require('@savvywombat/tailwindcss-grid-areas')],
 };
