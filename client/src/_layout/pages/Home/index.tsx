373c66f4065da6df691fd174d6590d53ea484321
youthfulhps <ybh942002@gmail.com>
2022-11-16 00:18:14 +0900

    Feature: Apply language section

--- client/src/_layout/pages/Home/index.tsx
+++ client/src/_layout/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import GridMain from '@layout/components/GridMain';
 import SuspenseBoundary from '@shared/boundaries/SuspenseBoundary';
 import Login from '../../../Login/components';
 import UserProfile from '../../../UserProfile/components';
+import Language from '../../../Language/components';
 
 function HomePage() {
   return (
@@ -9,6 +10,9 @@ function HomePage() {
       <Login />
       <SuspenseBoundary gridArea="user-profile">
         <UserProfile />
+      </SuspenseBoundary>
+      <SuspenseBoundary gridArea="language">
+        <Language />
       </SuspenseBoundary>
     </GridMain>
   );
