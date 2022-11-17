2fec2ffe1aaa85a0fecb18974871346ab939ab71
youthfulhps <ybh942002@gmail.com>
2022-11-17 11:56:58 +0900

    Setting: Modified jest config to use babel setting

--- client/jest.config.ts
+++ client/jest.config.ts
@@ -1,11 +1,24 @@
-import type { JestConfigWithTsJest } from "ts-jest";
+import type { JestConfigWithTsJest } from 'ts-jest';
 
 const config: JestConfigWithTsJest = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@layout(.*)$': '<rootDir>/src/_layout/$1',
+    '^@shared(.*)$': '<rootDir>/src/_shared/$1',
+  },
+
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.json',
+        babelConfig: '<rootDir>/babel.config.js',
+        useESM: true,
+      },
+    ],
   },
 };
 
