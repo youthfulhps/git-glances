b36ef62cb50675bf8dd9c1da356ceb14e12697c8
youthfulhps <ybh942002@gmail.com>
2022-11-29 13:12:59 +0900

    Feature: Implement GET repo list api

---
+++ client/src/_shared/apis/repo/queries.ts
@@ -0,0 +1,36 @@
+export const GET_REPO_LIST_QUERY = `
+query {
+  viewer {
+    repositories(first: 50, isFork: false, orderBy: {field: PUSHED_AT, direction: DESC}) {
+      nodes {
+        url
+        name
+        description
+        pushedAt
+        updatedAt
+        defaultBranchRef {
+          name
+          target {
+            ... on Commit {
+              history(first: 1) {
+                totalCount
+                nodes {
+                  ... on Commit {
+                    committedDate
+                    message
+                    additions
+                    author {
+                      name
+                      email
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }    
+  }
+}
+`;
