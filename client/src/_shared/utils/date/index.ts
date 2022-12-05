cb0386b6c78d4af2e216f567b7b02950a8f6112c
youthfulhps <ybh942002@gmail.com>
2022-12-06 00:49:30 +0900

    Feature: Implement date utils to handle today date

--- client/src/_shared/utils/date/index.ts
+++ client/src/_shared/utils/date/index.ts
@@ -1,6 +1,18 @@
-import { GetDiffDaysFromNow } from '@shared/utils/date/types';
+import {
+  GetDateAfterDays,
+  GetDiffDaysFromNow,
+  GetTodayDate,
+} from '@shared/utils/date/types';
 import moment from 'moment';
 
 export const getDiffDaysFromNow: GetDiffDaysFromNow = (target) => {
   return moment().diff(moment(target), 'days');
+};
+
+export const getTodayDateTime: GetTodayDate = () => {
+  return moment().startOf('day').format();
+};
+
+export const getDateTimeAfterDays: GetDateAfterDays = (days: number) => {
+  return moment().startOf('day').add(days, 'days').format();
 };
