ecdbf270f653d625d7dd825f85995a42e03bfc7c
youthfulhps <ybh942002@gmail.com>
2022-12-15 00:54:09 +0900

    Enhance: Apply recoil root at App

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RecoilRoot } from 'recoil';
 import App from './App';
 import './_shared/styles/index.css';
 
@@ -22,7 +23,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
