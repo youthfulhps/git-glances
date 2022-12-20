5e1c36212633953ad202df7764dc88bbafdce5d0
youthfulhps <ybh942002@gmail.com>
2022-12-20 14:14:50 +0900

    Test: Add contribution helper test cases

---
+++ client/src/Contribution/test/mocks.ts
@@ -0,0 +1,21 @@
+export const mockedContribution = {
+  data: {
+    viewer: {
+      contributionsCollection: {
+        totalCommitContributions: 1,
+        totalIssueContributions: 0,
+        totalPullRequestContributions: 2,
+        totalPullRequestReviewContributions: 0,
+        contributionCalendar: { totalContributions: 3 },
+      },
+    },
+  },
+};
+
+export const mockedContributionCollection = {
+  totalCommitContributions: 1,
+  totalIssueContributions: 0,
+  totalPullRequestContributions: 2,
+  totalPullRequestReviewContributions: 0,
+  contributionCalendar: { totalContributions: 3 },
+};
