6679f87026c4aa0f773fc00f6f861b8e911c29bb
youthfulhps <ybh942002@gmail.com>
2022-12-24 00:54:11 +0900

    Test: Fix contribution test failure cases caused by contribution enhancement

--- client/src/Contribution/test/Contribution.spec.tsx
+++ client/src/Contribution/test/Contribution.spec.tsx
@@ -1,26 +1,27 @@
 import { render, screen } from '@testing-library/react';
 import Contribution from '../components';
-import useContributionQuery from '../queries/useContributionQuery';
+import useContributionsCollectionQuery from '../queries/useContributionsCollectionQuery';
 import { mockedContributionCollection } from './mocks';
 
-const mockedUseContributionQuery = useContributionQuery as jest.Mock<any>;
+const mockedUseContributionQuery =
+  useContributionsCollectionQuery as jest.Mock<any>;
 
-jest.mock('../queries/useContributionQuery');
+jest.mock('../queries/useContributionsCollectionQuery');
 
 describe('Contribution 컴포넌트는 유저의 오늘 기여도 정보를 랜더링한다.', () => {
   beforeEach(() => {
-    mockedUseContributionQuery.mockImplementation(() => ({
-      contribution: mockedContributionCollection,
-    }));
+    mockedUseContributionQuery.mockImplementation(
+      () => mockedContributionCollection
+    );
   });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it('유저의 오늘 기여도 정보를 랜더링한다.', async () => {
+  it('유저의 총 기여도는 섹션 요약 정보에서 제공한다.', async () => {
     render(<Contribution />);
-    const contributionCount = await screen.findByText('3');
-    expect(contributionCount).toBeInTheDocument();
+    const totalContributions = await screen.findByText('3');
+    expect(totalContributions).toBeInTheDocument();
   });
 });
