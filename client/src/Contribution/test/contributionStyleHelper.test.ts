4bf508c533ed747f4578be3c49ece6a2d28f3884
youthfulhps <ybh942002@gmail.com>
2022-12-24 00:54:11 +0900

    Test: Fix contribution test failure cases caused by contribution enhancement

--- client/src/Contribution/test/contributionStyleHelper.test.ts
+++ client/src/Contribution/test/contributionStyleHelper.test.ts
@@ -2,10 +2,20 @@ import { getContributionLevelBackgroundClass } from '../utils/contributionStyleH
 
 describe('contributionStyleHelper는 기여도에 대한 스타일을 반환한다.', () => {
   it('getContributionLevelBackgroundClass는 기여도에 대한 배경 색상 클래스를 반환한다.', () => {
-    expect(getContributionLevelBackgroundClass(0)).toBe('bg-contribution-000');
-    expect(getContributionLevelBackgroundClass(3)).toBe('bg-contribution-100');
-    expect(getContributionLevelBackgroundClass(7)).toBe('bg-contribution-200');
-    expect(getContributionLevelBackgroundClass(17)).toBe('bg-contribution-300');
-    expect(getContributionLevelBackgroundClass(21)).toBe('bg-contribution-400');
+    expect(getContributionLevelBackgroundClass(0)).toBe(
+      'bg-contribution-000/[0.5]'
+    );
+    expect(getContributionLevelBackgroundClass(3)).toBe(
+      'bg-contribution-100/[0.5]'
+    );
+    expect(getContributionLevelBackgroundClass(7)).toBe(
+      'bg-contribution-200/[0.5]'
+    );
+    expect(getContributionLevelBackgroundClass(17)).toBe(
+      'bg-contribution-300/[0.5]'
+    );
+    expect(getContributionLevelBackgroundClass(21)).toBe(
+      'bg-contribution-400/[0.5]'
+    );
   });
 });
