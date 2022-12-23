ecd95bba47cf69c071b881ba31ab3f499b335d97
youthfulhps <ybh942002@gmail.com>
2022-12-23 23:55:06 +0900

    Enhance: Rename contributions collection query

--- client/src/Contribution/queries/useContributionsCollectionQuery.ts
+++ client/src/Contribution/queries/useContributionsCollectionQuery.ts
@@ -1,13 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
-import { getContribution } from '@shared/apis/contribution';
+import { getContributionsCollection } from '@shared/apis/contribution';
 import { getDestructuredContribution } from '../utils/contributionHelper';
 
-const useContributionQuery = (from: string, to: string) => {
+const useContributionsCollectionQuery = (from: string, to: string) => {
   const { data: contribution } = useQuery({
     queryKey: ['contribution', from, to],
     refetchOnWindowFocus: true,
     queryFn: async () => {
-      const { data } = await getContribution(from, to);
+      const { data } = await getContributionsCollection(from, to);
       const destructuredContribution = getDestructuredContribution(data);
 
       return destructuredContribution;
@@ -17,4 +17,4 @@ const useContributionQuery = (from: string, to: string) => {
   return { contribution };
 };
 
-export default useContributionQuery;
+export default useContributionsCollectionQuery;
