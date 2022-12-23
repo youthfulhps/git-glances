50b514bce0df0e671559ff9df8c8c483aa0c4f91
youthfulhps <ybh942002@gmail.com>
2022-12-23 23:25:44 +0900

    Enhance: Modify contribution level background class utils return value

--- client/src/Contribution/utils/contributionStyleHelper/types.ts
+++ client/src/Contribution/utils/contributionStyleHelper/types.ts
@@ -1,6 +1,7 @@
 type ContributionLevel = '000' | '100' | '200' | '300' | '400';
 
-type ContributionLevelBackgroundClass = `bg-contribution-${ContributionLevel}`;
+type ContributionLevelBackgroundClass =
+  `bg-contribution-${ContributionLevel}/[0.5]`;
 
 export type GetContributionLevelBackgroundClass = (
   totalContributions: number
