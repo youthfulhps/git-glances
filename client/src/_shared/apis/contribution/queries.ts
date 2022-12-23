c20af1f044521b0ae1676abeefe4138f65d9a219
youthfulhps <ybh942002@gmail.com>
2022-12-23 23:54:37 +0900

    Enhance: Rename GET contributions collection api

--- client/src/_shared/apis/contribution/queries.ts
+++ client/src/_shared/apis/contribution/queries.ts
@@ -1,4 +1,4 @@
-export const getContributionQuery = (from: string, to: string) => `
+export const getContributionsCollectionQuery = (from: string, to: string) => `
 query {
   viewer {
     contributionsCollection(from: "${from}", to: "${to}") {
