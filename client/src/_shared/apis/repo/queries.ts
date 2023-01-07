9fdd4d5f7e6814d2e3bd35073b5f4b58ef413f0e
youthfulhps <ybh942002@gmail.com>
2023-01-07 21:33:48 +0900

    Enhance: Apply defined repository list types to repository api

--- client/src/_shared/apis/repo/queries.ts
+++ client/src/_shared/apis/repo/queries.ts
@@ -23,6 +23,7 @@ query {
                     author {
                       name
                       email
+                      avatarUrl
                     }
                   }
                 }
