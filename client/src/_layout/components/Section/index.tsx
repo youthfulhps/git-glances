819dd8388cfcfa86c68879742ec2309548aeed2f
youthfulhps <ybh942002@gmail.com>
2023-01-17 01:08:58 +0900

    Enhance: Add section title to section component

--- client/src/_layout/components/Section/index.tsx
+++ client/src/_layout/components/Section/index.tsx
@@ -12,14 +12,23 @@ type SectionProps = {
 } & StyledSectionProps;
 
 const StyledSection = styled.section<StyledSectionProps>`
-  ${tw`h-full w-full rounded-2xl`}
-  ${tw`bg-zinc-800`}
+  ${tw`flex flex-col h-full w-full rounded-2xl`}
+  ${tw`bg-zinc-800 text-zinc-100`}
+  
   grid-area: ${({ gridArea }) => `${gridArea}`};
+
+  h2 {
+    ${tw`absolute top-4 left-4`}
+    ${tw`text-xl font-thin`}
+    ${tw`duration-500`}
+    ${tw`drop-shadow-md`}
+  }
 `;
 
 function Section({ children, className, gridArea }: SectionProps) {
   return (
     <StyledSection className={className} gridArea={gridArea}>
+      <h2>{`# ${gridArea}`}</h2>
       {children}
     </StyledSection>
   );
