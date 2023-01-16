0a3226978ceb159e848955086fef696509a274ff
youthfulhps <ybh942002@gmail.com>
2023-01-17 00:57:08 +0900

    Feature: Implement GET repo

--- client/src/_shared/apis/repo/apis.ts
+++ client/src/_shared/apis/repo/apis.ts
@@ -1,10 +1,18 @@
 import { axiosInstance } from '@shared/apis';
-import { GetRepoList } from '@shared/apis/repo/types';
-import { GET_REPO_LIST_QUERY } from './queries';
+import { GetRepo, GetRepoList } from '@shared/apis/repo/types';
+import { GET_REPO_LIST_QUERY, getRepoQuery } from './queries';
 
 export const getRepoList: GetRepoList = () => {
   const body = {
     query: GET_REPO_LIST_QUERY,
+  };
+
+  return axiosInstance.post('/graphql', body);
+};
+
+export const getRepo: GetRepo = (repoName) => {
+  const body = {
+    query: getRepoQuery(repoName),
   };
 
   return axiosInstance.post('/graphql', body);
