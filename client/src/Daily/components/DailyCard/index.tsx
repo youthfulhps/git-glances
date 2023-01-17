02cf0c35b95e0f8098556491c7826d077745bb0f
youthfulhps <ybh942002@gmail.com>
2023-01-17 14:54:19 +0900

    Feature: Implement initialization feature so that daily repo could be re-selected

--- client/src/Daily/components/DailyCard/index.tsx
+++ client/src/Daily/components/DailyCard/index.tsx
@@ -2,14 +2,25 @@ import React from 'react';
 import { Repository } from '@shared/apis/repo';
 import FeatureSection from '@layout/components/FeatureSection';
 import RepositoryDetail from '@shared/components/Details/Repository';
-import { CheckCircleFillIcon, XCircleFillIcon } from '@primer/octicons-react';
+import {
+  CheckCircleFillIcon,
+  XCircleFillIcon,
+  RepoDeletedIcon,
+} from '@primer/octicons-react';
 
 type DailyProps = {
-  dailyRepo: Repository;
+  dailyRepo: Repository | null;
   hasTodayContribution: boolean;
+  resetDailyRepo: () => void;
 };
 
-function DailyCard({ dailyRepo, hasTodayContribution }: DailyProps) {
+function DailyCard({
+  dailyRepo,
+  hasTodayContribution,
+  resetDailyRepo,
+}: DailyProps) {
+  if (!dailyRepo) return null;
+
   const summaryIcon = hasTodayContribution ? (
     <CheckCircleFillIcon size={48} className="fill-emerald-500" />
   ) : (
@@ -19,6 +30,13 @@ function DailyCard({ dailyRepo, hasTodayContribution }: DailyProps) {
   return (
     <FeatureSection summary={summaryIcon} summaryType="icon" gridArea="Daily">
       <RepositoryDetail repository={dailyRepo} />
+      <button
+        onClick={resetDailyRepo}
+        className="mt-3 flex w-full cursor-pointer items-center justify-end text-[8px] text-zinc-400 hover:text-zinc-200"
+      >
+        <RepoDeletedIcon size={12} className="mr-1" />
+        <span>Reset daily repository</span>
+      </button>
     </FeatureSection>
   );
 }
