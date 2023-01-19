06f7aed8c798eee77e9520e683f2083b31f70926
youthfulhps <ybh942002@gmail.com>
2023-01-19 15:47:13 +0900

    Style: Add font family at tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      alfa: ['Montagu Slab', 'sans-serif'],
+      fira: ['Fira Sans', 'sans-serif'],
+    },
     extend: {
       keyframes: {
         rising: {
