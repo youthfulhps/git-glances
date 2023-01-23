8acadd347cc5da4016aca4b372cfa42f37103276
youthfulhps <ybh942002@gmail.com>
2023-01-23 23:18:45 +0900

    Feature: Create trends repository list component

---
+++ client/src/Trends/components/TrendsRepoList/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { TrendsRepository } from '@shared/apis/repo';
+import TrendsRepoCard from './TrendsRepoCard';
+
+type TrendsCardProps = {
+  trendsRepoList: TrendsRepository[];
+};
+
+function TrendsCard({ trendsRepoList }: TrendsCardProps) {
+  return (
+    <ul className="my-1 h-[156px] overflow-y-scroll">
+      {trendsRepoList.map((trendsRepo) => (
+        <TrendsRepoCard key={trendsRepo.name} trendsRepo={trendsRepo} />
+      ))}
+    </ul>
+  );
+}
+
+export default TrendsCard;
