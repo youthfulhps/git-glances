cf0458fd1fdb8143283b4fa4ede13bee92ea4183
youthfulhps <ybh942002@gmail.com>
2023-01-24 01:22:43 +0900

    Style: Expand anchor area of trends repository card component

--- client/src/Trends/components/TrendsRepoList/TrendsRepoCard.tsx
+++ client/src/Trends/components/TrendsRepoList/TrendsRepoCard.tsx
@@ -8,24 +8,24 @@ type TrendsRepoCardProps = {
 
 function TrendsRepoCard({ trendsRepo }: TrendsRepoCardProps) {
   return (
-    <li className="flex w-full items-center justify-between py-2 text-start">
-      <div className="w-[calc(100%_-_48px)]">
-        <a
-          href={trendsRepo.url}
-          target="_blank"
-          className="mb-1 text-sm hover:text-zinc-400"
-          rel="noreferrer"
-        >
+    <li>
+      <a
+        href={trendsRepo.url}
+        target="_blank"
+        className="mb-1 flex w-full items-center justify-between py-2 text-start text-sm hover:text-zinc-400"
+        rel="noreferrer"
+      >
+        <div className="w-[calc(100%_-_48px)]">
           {trendsRepo.name}
-        </a>
-        <p className="truncate text-xs text-zinc-400">{trendsRepo.description}</p>
-      </div>
-      <div className="flex items-center text-xs">
-        <StarIcon size={10} className="mr-[2px]" />
-        <span className="mr-1">{trendsRepo.stargazers.totalCount}</span>
-        <RepoForkedIcon size={10} className="mr-[2px]" />
-        <span>{trendsRepo.forks.totalCount}</span>
-      </div>
+          <p className="truncate text-xs text-zinc-400">{trendsRepo.description}</p>
+        </div>
+        <div className="flex items-center text-xs">
+          <StarIcon size={10} className="mr-[2px]" />
+          <span className="mr-1">{trendsRepo.stargazers.totalCount}</span>
+          <RepoForkedIcon size={10} className="mr-[2px]" />
+          <span>{trendsRepo.forks.totalCount}</span>
+        </div>
+      </a>
     </li>
   );
 }
