3c2e78f9cadb8ec6ac77e76494845e457d0605c5
youthfulhps <ybh942002@gmail.com>
2023-01-23 23:15:30 +0900

    Feature: Implement GET trends repository list api

--- client/src/_shared/apis/types.ts
+++ client/src/_shared/apis/types.ts
@@ -1,11 +1,5 @@
 import { AxiosResponse } from 'axios';
 
-export type AsyncNestedAxiosResponse<Response> = Promise<
-  AxiosResponse<NestedFieldResponse<Response>>
->;
-
-export type AsyncAxiosResponse<Response> = Promise<AxiosResponse<Response>>;
-
 export type NestedFieldResponse<Response> = {
   data: {
     viewer: {
@@ -13,3 +7,25 @@ export type NestedFieldResponse<Response> = {
     };
   };
 };
+
+type SearchResultNode<Response> = {
+  node: Response;
+};
+
+export type ListSearchNestedFieldResponse<Response> = {
+  data: {
+    search: {
+      edges: SearchResultNode<Response>[];
+    };
+  };
+};
+
+export type AsyncNestedAxiosResponse<Response> = Promise<
+  AxiosResponse<NestedFieldResponse<Response>>
+>;
+
+export type AsyncListSearchNestedFieldResponse<Response> = Promise<
+  AxiosResponse<ListSearchNestedFieldResponse<Response>>
+>;
+
+export type AsyncAxiosResponse<Response> = Promise<AxiosResponse<Response>>;
