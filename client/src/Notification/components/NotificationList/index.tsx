5ff14d9502e8c29a41f3b182bba696ab59d14c59
youthfulhps <ybh942002@gmail.com>
2023-01-28 19:52:58 +0900

    Enhance: Implement to be rendered empty notification component if notification is empty

--- client/src/Notification/components/NotificationList/index.tsx
+++ client/src/Notification/components/NotificationList/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Notification } from '@shared/apis/notification/types';
 import NotificationCard from './NotificationCard';
+import EmptyNotification from './EmptyNotification';
 
 type NotificationListProps = {
   notificationList: Notification[];
+  isNotificationEmpty: boolean;
 };
 
-function NotificationList({ notificationList }: NotificationListProps) {
+function NotificationList({ notificationList, isNotificationEmpty }: NotificationListProps) {
+  if (isNotificationEmpty) return <EmptyNotification />;
+
   return (
     <ul className="my-1 h-[130px] overflow-y-scroll">
       {notificationList.map((notification) => (
