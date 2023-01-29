a5eae13e24b73e9121c6c11acc247195c819c741
youthfulhps <ybh942002@gmail.com>
2023-01-29 22:17:41 +0900

    Fix: Modify section component layout to center alignment

--- client/src/_layout/components/Section/index.tsx
+++ client/src/_layout/components/Section/index.tsx
@@ -12,18 +12,20 @@ type SectionProps = {
 } & StyledSectionProps;
 
 const StyledSection = styled.section<StyledSectionProps>`
-  ${tw`flex flex-col h-full w-full p-4 rounded-2xl overflow-hidden`}
+  ${tw`relative flex flex-col h-full w-full p-4 rounded-2xl overflow-hidden`}
   ${tw`bg-zinc-800 text-zinc-100`}
   
   grid-area: ${({ gridArea }) => `${gridArea}`};
 
   h2 {
+    ${tw`absolute top-4 left-4`}
     ${tw`text-xl font-thin`}
     ${tw`drop-shadow-md`}
   }
 
   .inner {
-    ${tw`w-full h-full overflow-y-scroll`}
+    ${tw`w-full h-full`}
+    ${tw`flex flex-col items-center justify-center`}
   }
 `;
 
