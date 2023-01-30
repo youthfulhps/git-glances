5a5a374e58421d07a74e951e4b198add2c96780b
youthfulhps <ybh942002@gmail.com>
2023-01-30 23:11:21 +0900

    Fix: Resolve issues where data is not fetched on initial login

--- client/src/Contribution/queries/useContributionsCollectionQuery.ts
+++ client/src/Contribution/queries/useContributionsCollectionQuery.ts
@@ -1,11 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { ContributionsCollection, getContributionsCollection } from '@shared/apis/contribution';
 import { AxiosError } from 'axios';
+import useLogin from '@shared/hooks/useLogin';
 import { getDestructuredContributionsCollection } from '../utils/contributionHelper';
 
 const useContributionsCollectionQuery = (from: string, to: string) => {
+  const { isLoggedIn } = useLogin();
+
   const { data: contributionsCollection } = useQuery<ContributionsCollection, AxiosError>({
-    queryKey: ['contributionsCollection', from, to],
+    queryKey: ['contributionsCollection', from, to, isLoggedIn],
     refetchOnWindowFocus: true,
     queryFn: async () => {
       const { data } = await getContributionsCollection(from, to);
