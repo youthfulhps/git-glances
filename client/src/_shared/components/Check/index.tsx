47ef67c3627a50db9ea38865e03e04b116c41de3
youthfulhps <ybh942002@gmail.com>
2023-01-30 16:28:15 +0900

    Feature: Create common success check component

---
+++ client/src/_shared/components/Check/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Section from '@layout/components/Section';
+import { CheckCircleFillIcon } from '@primer/octicons-react';
+
+type CheckProps = {
+  gridArea?: string;
+};
+
+function Check({ gridArea = '' }: CheckProps) {
+  return (
+    <Section gridArea={gridArea} className="flex h-full w-full items-center justify-center">
+      <CheckCircleFillIcon size={48} className="animate-rising fill-emerald-500" />
+    </Section>
+  );
+}
+
+export default Check;
