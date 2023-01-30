12af7f79bd81cd9ee7048abf4d7a37780f3b6484
youthfulhps <ybh942002@gmail.com>
2023-01-30 16:26:59 +0900

    Enhance: Add input hook return props

--- client/src/_shared/hooks/useInput.ts
+++ client/src/_shared/hooks/useInput.ts
@@ -1,13 +1,23 @@
-import { ChangeEventHandler, useState } from 'react';
+import { ChangeEventHandler, KeyboardEventHandler, useState } from 'react';
 
-const useInput = (initialValue: string) => {
+const useInput = (initialValue: string, onEnterKeyPress?: () => void) => {
   const [value, setValue] = useState(initialValue);
 
-  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+  const onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = (e) => {
     setValue(e.target.value);
   };
 
-  return { value, setValue, onChange };
+  const onKeyDown: KeyboardEventHandler<HTMLInputElement | HTMLTextAreaElement> = (e) => {
+    if (e.key === 'Enter' && onEnterKeyPress) {
+      onEnterKeyPress();
+    }
+  };
+
+  const resetValue = () => {
+    setValue('');
+  };
+
+  return { value, setValue, onChange, onKeyDown, resetValue };
 };
 
 export default useInput;
