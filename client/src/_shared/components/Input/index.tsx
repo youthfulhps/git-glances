390de678ba3e49479e2945cbed9f02508753ceb0
youthfulhps <ybh942002@gmail.com>
2023-02-02 01:33:20 +0900

    Enhance: Implement input component to expose arrow icon when input value present

--- client/src/_shared/components/Input/index.tsx
+++ client/src/_shared/components/Input/index.tsx
@@ -1,13 +1,27 @@
 import React, { InputHTMLAttributes } from 'react';
+import { ArrowRightIcon } from '@primer/octicons-react';
 
-type InputProps = InputHTMLAttributes<HTMLInputElement>;
+type InputProps = {
+  isArrowShowing?: boolean;
+  onArrowClick?: () => void;
+} & InputHTMLAttributes<HTMLInputElement>;
 
-function Input({ className, ...rest }: InputProps) {
+function Input({ isArrowShowing = false, onArrowClick, className, ...rest }: InputProps) {
   return (
-    <input
-      className={`w-full rounded-xl bg-zinc-800 p-3 font-light text-zinc-400 shadow-lg outline-0 ${className}`}
-      {...rest}
-    />
+    <div className="relative flex w-full items-center">
+      <input
+        className={`w-full rounded-xl bg-zinc-800 p-3 font-light text-zinc-400 shadow-lg outline-0 ${className}`}
+        {...rest}
+      />
+      {isArrowShowing && onArrowClick ? (
+        <button onClick={onArrowClick} tabIndex={-1}>
+          <ArrowRightIcon
+            size={20}
+            className="absolute top-3 right-4 animate-rising fill-emerald-300 hover:fill-emerald-200"
+          />
+        </button>
+      ) : null}
+    </div>
   );
 }
 
