307b25f28311901dda9e35e542b21eaaffe973d0
youthfulhps <ybh942002@gmail.com>
2023-02-02 16:00:27 +0900

    Feature: Apply logout component to header

--- client/src/_layout/components/Header/index.tsx
+++ client/src/_layout/components/Header/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Search from '../../../Search/components';
+import Logout from '../../../Logout/components';
 
 function Header() {
   return (
-    <header className="mb-2 flex h-[60px] w-full items-center justify-end rounded-2xl text-zinc-100">
+    <header className="mb-2 flex h-[60px] w-full items-center justify-between rounded-2xl text-zinc-100">
       <Search />
+      <Logout />
     </header>
   );
 }
