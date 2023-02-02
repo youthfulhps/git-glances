831d69f3aad006055b2e5106be68c70b6fc280a7
youthfulhps <ybh942002@gmail.com>
2023-02-03 01:20:44 +0900

    Style: Modify logout badge component hover styles

--- client/src/Logout/components/LogoutBadge.tsx
+++ client/src/Logout/components/LogoutBadge.tsx
@@ -14,9 +14,10 @@ function LogoutBadge({ onLogout, isLoggedIn }: LogoutBadgeProps) {
   return (
     <button
       onClick={onLogout}
-      className="group flex h-[59px] items-center rounded-2xl bg-zinc-800 p-2"
+      className="group flex h-[58px] w-[40px] items-center overflow-hidden rounded-2xl bg-zinc-800 p-2 duration-500 hover:w-[102px]"
     >
-      <SignOutIcon size={32} className="fill-zinc-400 group-hover:fill-zinc-100" />
+      <SignOutIcon size={20} className="ml-[2px] mr-3 fill-zinc-400 group-hover:fill-zinc-100" />
+      <span className="text-base font-thin text-zinc-400 drop-shadow-md">Logout</span>
     </button>
   );
 }
