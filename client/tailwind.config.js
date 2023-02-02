a7a9a4ae72b4e1c16bd0b2b05944050a704fd794
youthfulhps <ybh942002@gmail.com>
2023-02-03 01:04:00 +0900

    Style: Add tailwind screens theme

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,14 @@ module.exports = {
     scrollbar: ['rounded'],
   },
   theme: {
+    screens: {
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+      md: '768px',
+      // => @media (min-width: 768px) { ... }
+      lg: '1024px',
+      // => @media (min-width: 1024px) { ... }
+    },
     fontFamily: {
       alfa: ['Montagu Slab', 'sans-serif'],
       fira: ['Fira Sans', 'sans-serif'],
