4cada888474c0b3f7dc2df194b573086e72d3031
youthfulhps <ybh942002@gmail.com>
2023-02-05 18:28:15 +0900

    Feature: Apply guide component

--- client/src/_layout/components/Header/index.tsx
+++ client/src/_layout/components/Header/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Search from '../../../Search/components';
 import Logout from '../../../Logout/components';
+import Guide from '../../../Guide/components';
 
 function Header() {
   return (
     <header className="mb-2 flex h-[60px] w-full items-center justify-between rounded-2xl text-zinc-100">
       <Search />
-      <Logout />
+      <div className="item-center flex">
+        <Guide />
+        <div className="mr-2" />
+        <Logout />
+      </div>
     </header>
   );
 }
