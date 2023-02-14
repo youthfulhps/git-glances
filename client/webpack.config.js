1c3f53be126972ecf569ce8e4a3523c35ca40881
youthfulhps <ybh942002@gmail.com>
2023-02-15 00:32:01 +0900

    POC: Modify webpack config to accept env variables

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,85 +6,92 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const isProd = process.env.NODE_ENV === 'production';
 const PORT = process.env.PORT || 3000;
 
-module.exports = {
-  mode: isProd ? 'production' : 'development',
-  devtool: isProd ? 'hidden-source-map' : 'source-map',
-  entry: './src/index.tsx',
-  output: {
-    filename: '[name].js',
-    path: path.join(__dirname, '/dist'),
-    publicPath: '/',
-  },
-  resolve: {
-    modules: ['node_modules'],
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    alias: {
-      '@shared': path.resolve(__dirname, './src/_shared'),
-      '@layout': path.resolve(__dirname, './src/_layout'),
+module.exports = ({ ENV, TARGET }) => {
+  const isProd = ENV === 'production';
+
+  return {
+    mode: isProd ? 'production' : 'development',
+    devtool: isProd ? 'hidden-source-map' : 'source-map',
+    entry: './src/index.tsx',
+    output: {
+      filename: '[name].js',
+      path: path.join(__dirname, '/dist'),
+      publicPath: '/',
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        loader: 'ts-loader',
-        options: {
-          transpileOnly: !isProd,
-        },
+    resolve: {
+      modules: ['node_modules'],
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      alias: {
+        '@shared': path.resolve(__dirname, './src/_shared'),
+        '@layout': path.resolve(__dirname, './src/_layout'),
       },
-      {
-        test: /\.css?$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.(webp|jpg|png|jpeg)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]?[hash]',
-        },
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html'),
-      hash: false,
-    }),
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(process.env),
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
+    },
+    module: {
+      rules: [
         {
-          from: path.resolve(__dirname, 'public/*.png'),
-          to() {
-            return '[name][ext]';
+          test: /\.(ts|tsx)$/,
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: !isProd,
           },
         },
         {
-          from: path.resolve(__dirname, 'public/icons'),
-          to() {
-            return 'icons/[name][ext]';
+          test: /\.css?$/,
+          use: ['style-loader', 'css-loader', 'postcss-loader'],
+        },
+        {
+          test: /\.(webp|jpg|png|jpeg)$/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]?[hash]',
           },
+        },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
         },
       ],
-    }),
-  ],
-  devServer: {
-    host: 'localhost',
-    port: PORT,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-  },
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'public', 'index.html'),
+        hash: false,
+      }),
+      new webpack.DefinePlugin({
+        'process.env': JSON.stringify(process.env),
+        'process.env.TARGET': JSON.stringify(TARGET),
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'public/*.png'),
+            to() {
+              return '[name][ext]';
+            },
+          },
+          {
+            from: path.resolve(__dirname, 'public/icons'),
+            to() {
+              return 'icons/[name][ext]';
+            },
+          },
+        ],
+      }),
+    ],
+    experiments: {
+      topLevelAwait: true,
+    },
+    devServer: {
+      host: 'localhost',
+      port: PORT,
+      open: true,
+      hot: true,
+      compress: true,
+      historyApiFallback: true,
+    },
+  };
 };
