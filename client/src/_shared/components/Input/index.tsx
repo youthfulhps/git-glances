f631def640e8b102f5061c255dd5d11795be95c6
youthfulhps <ybh942002@gmail.com>
2023-02-21 18:26:49 +0900

    POC: Change common input component styles

--- client/src/_shared/components/Input/index.tsx
+++ client/src/_shared/components/Input/index.tsx
@@ -10,14 +10,14 @@ function Input({ isArrowShowing = false, onArrowClick, className, ...rest }: Inp
   return (
     <div className="relative flex w-full items-center">
       <input
-        className={`w-full rounded-xl bg-zinc-800 p-3 font-light text-zinc-400 shadow-lg outline-0 ${className}`}
+        className={`w-full rounded-xl bg-zinc-800 p-3 pr-[32px] font-light text-zinc-400 shadow-lg outline-0 ${className}`}
         {...rest}
       />
       {isArrowShowing && onArrowClick ? (
         <button onClick={onArrowClick} tabIndex={-1}>
           <ArrowRightIcon
             size={20}
-            className="absolute top-3 right-4 animate-rising fill-emerald-300 hover:fill-emerald-200"
+            className="absolute top-3 right-3 animate-rising bg-gradient-to-r from-transparent to-zinc-800 fill-emerald-300 hover:fill-emerald-200"
           />
         </button>
       ) : null}
