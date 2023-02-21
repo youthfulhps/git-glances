d3dd7fc3bed9f8ad851b571723e74d8fc8454dbd
youthfulhps <ybh942002@gmail.com>
2023-02-21 18:36:03 +0900

    POC: Implement token input feature

--- client/src/Login/components/ExtensionLogin.tsx
+++ client/src/Login/components/ExtensionLogin.tsx
@@ -3,7 +3,10 @@ import Input from '@shared/components/Input';
 import useLogin from '@shared/hooks/useLogin';
 
 function ExtensionLogin() {
-  const { tokenForExtension, onTokenForExtensionChange, submitTokenForExtension } = useLogin();
+  const { inputToken, onInputTokenChange, submitInputToken, isLoggedIn, onInputTokenKeyDown } =
+    useLogin();
+
+  if (isLoggedIn) return null;
 
   return (
     <section className="flex h-full w-full flex-col items-center rounded-2xl border-2 border-solid border-zinc-100 p-4">
@@ -11,10 +14,11 @@ function ExtensionLogin() {
         <LogoGithub size={32} color="#F5F4F5" className="mr-2" />
         <Input
           placeholder="Enter token..."
-          value={tokenForExtension}
-          onChange={onTokenForExtensionChange}
-          isArrowShowing={!!tokenForExtension}
-          onArrowClick={submitTokenForExtension}
+          value={inputToken}
+          onChange={onInputTokenChange}
+          onKeyDown={onInputTokenKeyDown}
+          isArrowShowing={!!inputToken}
+          onArrowClick={submitInputToken}
         />
       </div>
     </section>
