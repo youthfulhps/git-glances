8f4815a8c47f1fbb1e554a28be9281b3740f7841
youthfulhps <ybh942002@gmail.com>
2023-09-21 14:30:03 +0900

    Feature: Add google app badge for meeting

--- client/src/GoogleApp/components/index.tsx
+++ client/src/GoogleApp/components/index.tsx
@@ -1,18 +1,20 @@
 import GoogleAppBadge from './GoogleAppBadge';
 
 function GoogleApp() {
-  const handleBadgeClick = (target: 'mail' | 'calendar') => {
-    window.open(`https://${target}.google.com/${target}`, '_blank');
+  const handleBadgeClick = (target: 'mail' | 'calendar' | 'meet') => {
+    window.open(`https://${target}.google.com`, '_blank');
   };
 
   return (
     <section className="flex h-full items-center">
-      <GoogleAppBadge onClick={() => handleBadgeClick('mail')} target="mail" className="mr-2" />
-      <GoogleAppBadge
-        onClick={() => handleBadgeClick('calendar')}
-        target="calendar"
-        className="mr-2"
-      />
+      {(['mail', 'calendar', 'meet'] as const).map((target) => (
+        <GoogleAppBadge
+          key="target"
+          onClick={() => handleBadgeClick(target)}
+          target={target}
+          className="mr-2"
+        />
+      ))}
     </section>
   );
 }
