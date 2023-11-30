67c5b1d36c981eed05c6d76c22c82756620b81e8
youthfulhps <ybh942002@gmail.com>
2023-11-30 15:17:06 +0900

    Feature: Modify sammary icon size on daily card

--- client/src/Daily/components/DailyCard/index.tsx
+++ client/src/Daily/components/DailyCard/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { Repository } from '@shared/apis/repo';
 import FeatureSection from '@layout/components/FeatureSection';
 import RepositoryDetail from '@shared/components/Details/Repository';
-import {
-  CheckCircleFillIcon,
-  XCircleFillIcon,
-  RepoDeletedIcon,
-} from '@primer/octicons-react';
+import { CheckCircleFillIcon, XCircleFillIcon, RepoDeletedIcon } from '@primer/octicons-react';
 
 type DailyProps = {
   dailyRepo: Repository | null;
@@ -14,17 +10,13 @@ type DailyProps = {
   resetDailyRepo: () => void;
 };
 
-function DailyCard({
-  dailyRepo,
-  hasTodayContribution,
-  resetDailyRepo,
-}: DailyProps) {
+function DailyCard({ dailyRepo, hasTodayContribution, resetDailyRepo }: DailyProps) {
   if (!dailyRepo) return null;
 
   const summaryIcon = hasTodayContribution ? (
-    <CheckCircleFillIcon size={48} className="fill-emerald-500" />
+    <CheckCircleFillIcon size={40} className="fill-emerald-500" />
   ) : (
-    <XCircleFillIcon size={48} className="fill-red-400" />
+    <XCircleFillIcon size={40} className="fill-red-400" />
   );
 
   return (
