cafb14c68d6b03493cdff674af02a4fae2d0527c
youthfulhps <ybh942002@gmail.com>
2023-12-02 23:37:28 +0900

    Refactor: Redefine shared section component styles

--- client/src/_layout/components/Section/index.tsx
+++ client/src/_layout/components/Section/index.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react';
-import styled from 'styled-components';
-import tw from 'twin.macro';
+import classNames from 'classnames';
 
 type StyledSectionProps = {
   gridArea?: string;
@@ -11,31 +10,18 @@ type SectionProps = {
   className?: string;
 } & StyledSectionProps;
 
-const StyledSection = styled.section<StyledSectionProps>`
-  ${tw`relative flex flex-col h-full w-full p-4 rounded-2xl overflow-hidden`}
-  ${tw`text-zinc-100`}
-  ${tw`border border-solid border-zinc-500`}
-  
-  grid-area: ${({ gridArea }) => `${gridArea}`};
-
-  h2 {
-    ${tw`absolute top-4 left-4`}
-    ${tw`text-xl font-thin`}
-    ${tw`drop-shadow-md`}
-  }
-
-  .inner {
-    ${tw`w-full h-full`}
-    ${tw`flex flex-col items-center justify-center`}
-  }
-`;
-
 function Section({ children, className, gridArea }: SectionProps) {
   return (
-    <StyledSection className={className} gridArea={gridArea}>
-      <h2>{`# ${gridArea}`}</h2>
-      <div className="inner">{children}</div>
-    </StyledSection>
+    <section
+      className={classNames(
+        'relative flex h-full w-full flex-col overflow-hidden rounded-2xl border border-solid border-zinc-500 p-4 text-zinc-100',
+        className
+      )}
+      style={{ gridArea }}
+    >
+      <h2 className="absolute top-4 left-4 text-xl font-thin drop-shadow-md">{`# ${gridArea}`}</h2>
+      <div className="flex h-full w-full flex-col items-center justify-center">{children}</div>
+    </section>
   );
 }
 
