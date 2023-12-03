2f41adb63acbe4d17621707704eb030a0eeacb0b
youthfulhps <ybh942002@gmail.com>
2023-12-03 17:49:36 +0900

    Refactor: Redefine web login component styles

--- client/src/Login/components/WebLogin.tsx
+++ client/src/Login/components/WebLogin.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { LogoGithub } from '@carbon/icons-react';
-import styled from 'styled-components';
-import tw from 'twin.macro';
 import useLogin from '@shared/hooks/useLogin';
 
 const loginURLWithFullAccess =
@@ -16,28 +14,23 @@ const loginUrlWithPublicOnly =
   `&redirect_uri=${process.env.GITHUB_OAUTH_REDIRECT_URL}` +
   '&scope=notifications,public_repo,read:user,user:email,user:follow';
 
-const StyledLogoGithub = styled.a`
-  ${tw`flex items-center justify-center`}
-  ${tw`h-full w-full rounded-2xl`}
-  ${tw`bg-zinc-400`}
-  ${tw`hover:bg-zinc-100 transition-all`}
-`;
-
 function Login() {
   const { isLoggedIn } = useLogin();
 
   if (isLoggedIn) return null;
 
   return (
-    <section className="flex h-full w-full flex-col items-center justify-center rounded-2xl text-xs">
-      <StyledLogoGithub href={loginUrlWithPublicOnly} className="mb-2">
-        <LogoGithub size={32} className="mr-2" />
-        <span>Read public repositories only</span>
-      </StyledLogoGithub>
-      <StyledLogoGithub href={loginURLWithFullAccess}>
-        <LogoGithub size={32} className="mr-2" />
-        <span>Full control of private repositories</span>
-      </StyledLogoGithub>
+    <section className="flex h-full w-full flex-col items-center justify-center rounded-2xl text-xs [&>*:first-child]:mb-2">
+      {[loginUrlWithPublicOnly, loginURLWithFullAccess].map((href) => (
+        <a
+          key={href}
+          href={href}
+          className="flex h-full w-full items-center justify-center rounded-2xl bg-zinc-400 transition-all hover:bg-zinc-100"
+        >
+          <LogoGithub size={32} className="mr-2" />
+          <span>Read public repositories only</span>
+        </a>
+      ))}
     </section>
   );
 }
