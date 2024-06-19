0d022bc6c06fceb22e32fa355b6925bb46647939
youthfulhps <ybh942002@gmail.com>
2024-06-19 17:55:54 +0900

    Refactor: Replaced useQuery with useSuspenseQuery in useContributionsCollectionQuery

--- client/src/Contribution/queries/useContributionsCollectionQuery.ts
+++ client/src/Contribution/queries/useContributionsCollectionQuery.ts
@@ -1,21 +1,19 @@
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { ContributionsCollection, getContributionsCollection } from '@shared/apis/contribution';
 import { AxiosError } from 'axios';
 import { getDestructuredContributionsCollection } from '../utils/contributionHelper';
 
 const useContributionsCollectionQuery = (from: string, to: string) => {
-  const { data: contributionsCollection } = useQuery<ContributionsCollection, AxiosError>({
+  const { data: contributionsCollection } = useSuspenseQuery<ContributionsCollection, AxiosError>({
     queryKey: ['contributionsCollection', from, to],
     refetchOnWindowFocus: true,
     queryFn: async () => {
       const { data } = await getContributionsCollection(from, to);
-      const destructuredContributionsCollection = getDestructuredContributionsCollection(data);
-
-      return destructuredContributionsCollection;
+      return getDestructuredContributionsCollection(data);
     },
   });
 
-  return contributionsCollection as ContributionsCollection;
+  return contributionsCollection;
 };
 
 export default useContributionsCollectionQuery;
