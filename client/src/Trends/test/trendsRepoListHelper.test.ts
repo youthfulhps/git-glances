ff2db42811b1291fc2a4085a9536d113d6c059ed
youthfulhps <ybh942002@gmail.com>
2026-01-25 17:43:41 +0900

    test(trends): remove outdated Trends tests and add new components tests

--- client/src/Trends/test/trendsRepoListHelper.test.ts
+++ client/src/Trends/test/trendsRepoListHelper.test.ts
@@ -1,8 +1,8 @@
 import { getDestructuredTrendsRepoList } from '../utils/trendsRepoListHelper';
 import { mockedRawTrendsRepoList, mockedDestructuredTrendsRepoList } from './mocks';
 
-describe('trendsRepoListHelper는 전처리된 트렌드 레포지토리 결과를 반환한다.', () => {
-  it('getDestructuredTrendsRepoList는 중첩된 객체에서 트렌드 레포지토리들에 대한 정보를 반환한다.', () => {
+describe('trendsRepoListHelper', () => {
+  it('getDestructuredTrendsRepoList returns trend repositories from nested object', () => {
     expect(getDestructuredTrendsRepoList(mockedRawTrendsRepoList)).toStrictEqual(
       mockedDestructuredTrendsRepoList
     );
